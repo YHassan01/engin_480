999ec914bd7a2a6f8769f4dd340696efe67ae669
yoseph.hassan001@umb.edu <yoseph.hassan001@umb.edu>
2025-04-13 08:23:12 -0400

    cretaed project 3 code here

--- datatypes_structures.py
+++ datatypes_structures.py
@@ -1,84 +1,50 @@
-# ENGIN480 -  Special Topics: Introduction to Computational Modeling for Renewable Energy
-# Instructor: Rafael Valotta Rodrigues
-# March 25th, 2025
+import numpy as np
+import matplotlib.pyplot as plt
 
-# 1. Lists
-books = ['1984', '100 years of soletude', 'Blindness']
-numbers = [0, 2.309823498, 399329382]
+class WindFarmBoundary:
+    def __init__(self, name, coordinates):
+        """
+        Initializes the WindFarmBoundary object.
 
-# 1.1 printing the list
-print(books)
+        Args:
+            name (str): Name of the wind farm.
+            coordinates (list of tuples): List of (longitude, latitude) pairs.
+        """
+        self.name = name
+        self.coordinates = coordinates
+        self.array = np.array(coordinates)
 
-# 1.2 looping over it and printing each element
-for i in books:
-    print(i)
+    def get_array(self):
+        return self.array
 
-# or
+    def plot(self):
+        """
+        Plots the boundary using matplotlib.
+        """
+        x, y = self.array[:, 0], self.array[:, 1]
+        plt.figure(figsize=(6, 6))
+        plt.plot(x, y, marker='o', linestyle='-', color='blue')
+        plt.fill(x, y, color='skyblue', alpha=0.3)
+        plt.title(f"Boundary of {self.name}")
+        plt.xlabel("Longitude")
+        plt.ylabel("Latitude")
+        plt.grid(True)
+        plt.axis("equal")
+        plt.show()
 
-for k in range(len(books)):
-    print('Book:',books[k])
+# Example usage
+eystrasalt_coords = [
+    (17.004, 57.008),
+    (17.005, 57.005),
+    (17.005, 57.001),
+    (17.005, 56.998),
+    (17.006, 56.994),
+    (17.004, 57.008)  # close the polygon
+]
 
+eystrasalt = WindFarmBoundary("Eystrasalt", eystrasalt_coords)
+print("Numpy array of boundary:")
+print(eystrasalt.get_array())
 
-# 1.3 Replacing an element of the list:
-# 
-#  
-print('starting replacement on list')
-
-for k in range(len(books)):
-    if books[k] == 'Blindness':
-        books[k] = 'Notes From Underground'
-    print('Book:',books[k])
-
-# 2. Tuples
-numbers = (1, 4, 18, 20, 37, 70)
-
-# 2.1 printing the list
-print(numbers)
-
-# 2.2 Looping over it and printing each element
-for k in range(len(numbers)):
-    print(k)
-
-
-# 2.3 Attempting to replace an element from a tuple:
-print('starting attempt to replace element on tuple')
-
-for k in range(len(numbers)):
-    if numbers[k] == 18:
-        # numbers[k] = 19
-        pass
-    print('Number:', numbers[k])
-# it is not possible to replace elements in tuples, therefore there will be a bug here
- 
-
-# 3. Dictionaries
-tasks = {'morning'  :'wash', 
-         'lunch'    : 'walk the dog', 
-         'afternoon': 'go to school', 
-         'evening'  : 'workout'}
-
-print('')
-
-print('starting dictionaries')
-
-print(tasks.keys())
-
-print(tasks.get('morning'))
-print(tasks.get('lunch'))
-print(tasks.get('afternoon'))
-print(tasks.get('evening'))
-
-#or
-
-print(tasks.keys().mapping['morning'])
-print(tasks.keys().mapping['lunch'])
-print(tasks.keys().mapping['afternoon'])
-print(tasks.keys().mapping['evening'])
-
-
-print('done')
-print('done')
-print('done')
-
-
-#testing my code
+# Plot
+eystrasalt.plot()
